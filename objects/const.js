bffbe1215732e99bdad2335828a8e10b9cf25d62
Amar <sharewithamar@gmail.com>
2018-05-08 21:32:51 +0530

    const

---
+++ objects/const.js
@@ -0,0 +1,15 @@
+const isRaining = true
+
+//isRaining = false
+
+const person = {
+    age: 27
+}
+//person = {}
+
+person.age = 28
+console.log(person)
+
+//const cannot be reassinged - useful for semantic betterreadibilty purpose
+//constant varibale - object properties can be manipuilted 
+//let can be reassigned
