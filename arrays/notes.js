95efa11d4886fbac8e45a4d3604c28eae755ad41
Amar <sharewithamar@gmail.com>
2018-05-29 21:20:49 +0530

    search array

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,69 +1,115 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+// const notes = ['Note 1', 'Note 2', 'Note 3']
 
 //length property
-console.log(notes.length)
+//console.log(notes.length)
 
 //Grab by index position
-console.log(notes[0])
+//console.log(notes[0])
 
 //undefined
-console.log(notes[notes.length])
+//console.log(notes[notes.length])
 
 //Get last item
-console.log(notes[notes.length - 2])
+//console.log(notes[notes.length - 2])
 
 //array push
-notes.push('My new Note')
-console.log(notes)
+//notes.push('My new Note')
+//console.log(notes)
 
 //array pop - pop returns the removed value to store as well
 //notes.pop()
-console.log(notes.pop())
+//console.log(notes.pop())
 
 //array shift - removes the first item in the array
-console.log(notes.shift())
+//console.log(notes.shift())
 
 //array unshift  -add item in the beginning 
-console.log(notes.unshift('Amar'))
-console.log(notes)
+// console.log(notes.unshift('Amar'))
+// console.log(notes)
 
 //array splice  -add or remove or replace elements in middle of the array
 //remove
-notes.splice(1, 1)
-console.log(notes)
+// notes.splice(1, 1)
+// console.log(notes)
 
 //add
-notes.splice(1, 0, 'this is the new second item')
-console.log(notes)
+// notes.splice(1, 0, 'this is the new second item')
+// console.log(notes)
 
 //replace
 
-notes.splice(1, 1, 'this is the replaced item')
-console.log(notes)
+// notes.splice(1, 1, 'this is the replaced item')
+// console.log(notes)
 
 //reading/setting value with breacket notation
-notes[2] = 'new note bracket'
-console.log(notes)
+// notes[2] = 'new note bracket'
+// console.log(notes)
 
 
 //foreach -args individual item,index
 //ForEach can work only with exisiting data
 
-notes.forEach(function (item, index) {
-    console.log(index)
+// notes.forEach(function (item, index) {
+//     console.log(index)
 
-    console.log(item)
+//     console.log(item)
 
-})
+// })
 
 //for loop - counting ...1 up to 3
 //For loop can work without array data as well like just looping number counter
 // array.length - 1 // for descend loop
 
-for (let count = 0; count <= 2; count++) {
-    console.log(`counting...${count + 1}`);
-}
+// for (let count = 0; count <= 2; count++) {
+//     console.log(`counting...${count + 1}`);
+// }
 
-for (let count = 0; count < notes.length; count++) {
-    console.log(notes[count])
+// for (let count = 0; count < notes.length; count++) {
+//     console.log(notes[count])
+// }
+
+//indexof - returns index , or -1 if does not exist
+//console.log(notes.indexOf('new note bracket'))
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to spain'
+
+}, {
+    title: 'habits to work on',
+    body: 'Excercise Eating healthy Yoga'
+}, {
+
+    title: 'Office modification',
+    body: 'Learn new tech'
+}]
+
+//indexOf doesn't work with array of objects as they are different references
+//console.log(notes.indexOf({}))
+
+//using findIndex
+
+// const findNote = function (notes, noteTitle) {
+//     const index = notes.findIndex((note, index) => {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase()
+//     })
+//     return notes[index]
+// }
+
+//using find method  -returns undefined if no match found
+
+const findNote = function (notes, noteTitle) {
+    return notes.find((note, index) => {
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+
+
 }
+const note = findNote(notes, 'office modification')
+console.log(note)
+
+//findIndex
+// const index = notes.findIndex(function (note, index) {
+//     return note.title === 'habits to work on'
+// })
+// console.log(index)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,26 +1,50 @@
-const todos = ['js', 'Html5', 'css3', 'angular', 'polymer']
+// const todos = ['js', 'Html5', 'css3', 'angular', 'polymer']
 
-// console.log(todos[0])
-// console.log(`you have ${todos.length} todos`)
-// console.log(`Todo: ${todos[todos.length - 2]}`)
-console.log(todos)
+// // console.log(todos[0])
+// // console.log(`you have ${todos.length} todos`)
+// // console.log(`Todo: ${todos[todos.length - 2]}`)
+// console.log(todos)
 
-//delete 3rd item
-todos.splice(2, 1)
-console.log(todos)
+// //delete 3rd item
+// todos.splice(2, 1)
+// console.log(todos)
 
-//add a new item in the  end
-todos.push('new item')
-console.log(todos)
+// //add a new item in the  end
+// todos.push('new item')
+// console.log(todos)
 
-//remove first item in the list
-todos.shift()
+// //remove first item in the list
+// todos.shift()
 
-// foreach
+// // foreach
 
-todos.forEach((item, index) => console.log(`${index + 1}.${item}`))
+// todos.forEach((item, index) => console.log(`${index + 1}.${item}`))
 
-//using for
-for (let count = 0; count < todos.length; count++) {
-    console.log(`${count + 1}.${todos[count]}`)
+// //using for
+// for (let count = 0; count < todos.length; count++) {
+//     console.log(`${count + 1}.${todos[count]}`)
+// }
+
+//convert array in to array of objects - text,completed
+const todos = [{
+    text: 'HTML5',
+    completed: true
+}, {
+    text: 'Node',
+    completed: false
+}, {
+    text: 'Python',
+    completed: false
+}]
+
+//create function to remove a todo by text value
+const deleteTodo = function (todos, text) {
+
+    const index = todos.findIndex((todo) => todo.text.toLowerCase() === text.toLowerCase())
+    if (index > -1)
+        todos.splice(index, 1)
+
+
 }
+deleteTodo(todos, 'HTML5')
+console.log(todos)
