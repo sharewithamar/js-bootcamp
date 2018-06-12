8070e6c5568ade746e87607938cf92c14f8f7f33
Amar <sharewithamar@gmail.com>
2018-06-12 21:30:34 +0530

    creating and appending element

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,17 @@
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to spain'
+
+}, {
+    title: 'habits to work on',
+    body: 'Excercise Eating healthy Yoga'
+}, {
+
+    title: 'Office modification',
+    body: 'Learn new tech'
+}]
+
+
 //Document Object Model - document = html , object = js object(document)
 
 //Query and Remove 
@@ -12,3 +26,12 @@ ps.forEach(element => {
     //console.log(element.textContent)
     //  element.remove()
 });
+
+
+//add a new element
+
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'Noyan returns'
+document.querySelector('body').appendChild(newParagraph)
+
+
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,3 +1,35 @@
+const todos = [{
+    text: 'HTML5',
+    completed: true
+}, {
+    text: 'Node',
+    completed: false
+}, {
+    text: 'Python',
+    completed: false
+},
+{
+    text: 'CSS',
+    completed: false
+},
+{
+    text: 'React',
+    completed: false
+},
+{
+    text: 'Javascript',
+    completed: false
+},
+{
+    text: 'Angular',
+    completed: false
+},
+{
+    text: 'Polymer',
+    completed: false
+}]
+
+/*
 const p = document.querySelectorAll('p')
 
 p.forEach(element => {
@@ -9,4 +41,19 @@ p.forEach(element => {
         element.remove()
     }
 
+})
+*/
+
+//You have two todos left in a paragraph tag
+
+const todosLeft = todos.filter(todo => !todo.completed)
+
+const summary = document.createElement('h2')
+summary.textContent = `you have ${todosLeft.length} todos left`
+document.querySelector('body').appendChild(summary)
+//print a p tag for each todo above , textValue 
+todos.forEach(todo => {
+    let currentTodoElement = document.createElement('p')
+    currentTodoElement.textContent = todo.text
+    document.querySelector('body').appendChild(currentTodoElement)
 })
