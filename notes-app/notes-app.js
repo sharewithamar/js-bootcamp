3dbd2ef49f5e9db19896b9c3b0436db0d99dff49
Amar <sharewithamar@gmail.com>
2018-07-02 20:37:26 +0530

    local storage part1

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -50,6 +50,24 @@ const filters = {
     searchText: ''
 }
 
+//localStorage.setItem('location', 'Banglore')
+//console.log(localStorage.getItem('location'))
+
+//localStorage.removeItem('location')
+//localStorage.clear()
+
+/* const user = {
+    name: 'Amar',
+    age: 29
+}
+const userJSON = JSON.stringify(user)
+console.log(userJSON)
+localStorage.setItem('user', userJSON) */
+
+const userJSON = localStorage.getItem('user');
+const user = JSON.parse(userJSON)
+console.log(user.name)
+
 const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter((note) => {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
